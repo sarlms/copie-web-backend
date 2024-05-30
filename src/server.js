3f1e1e379ad6a00b6fb98f8a18c505b166afc138
unknown <sarah.elmss@icloud.com>
2024-05-30 16:23:07 +0200

    change cors origin

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ const io = socketIo(server, {
 
 // Configuration CORS pour Express
 app.use(cors({
-  origin: ['http://localhost:3001', 'https://sarargentique.cluster-ig3.igpolytech.fr', 'http://localhost:3000', 'https://sarargentique2.cluster-ig3.igpolytech.fr'],
+  origin: '*', //['http://localhost:3001', 'https://sarargentique.cluster-ig3.igpolytech.fr', 'http://localhost:3000', 'https://sarargentique2.cluster-ig3.igpolytech.fr'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true
 }));
